9162d6f670dc87ac362da0e6e64c492a2da9e134
Dino Damianoff <75574873+DDamianoff@users.noreply.github.com>
2022-11-02 21:48:30 -0300

    stuff

--- Guacamole.Models/Contexts/BluntContext.cs
+++ Guacamole.Models/Contexts/BluntContext.cs
@@ -4,11 +4,11 @@ namespace Guacamole.Models.Contexts;
 
 public class BluntContext : DbContext
 {
-    public System.Data.Entity.DbSet<Composer> Composers;
+    DbSet<Composer> Composers;
 
-    public System.Data.Entity.DbSet<MusicPiece> Pieces;
-    public System.Data.Entity.DbSet<Period> Periods;
-    public System.Data.Entity.DbSet<TimePeriod> TimePeriods;
+    DbSet<MusicPiece> Pieces;
+    DbSet<Period> Periods;
+    DbSet<TimePeriod> TimePeriods;
 
     public BluntContext (DbContextOptions<BluntContext> options)
         : base(options) 
