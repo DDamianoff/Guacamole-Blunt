a8e4d5fa5e1da23f1dd874e65e782e3842c48ec0
Dino Damianoff <75574873+DDamianoff@users.noreply.github.com>
2022-11-08 22:10:44 -0300

    EF helloworld

--- Guacamole.Minimal/Program.cs
+++ Guacamole.Minimal/Program.cs
@@ -1,6 +1,23 @@
+using Guacamole.Models.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BluntContext>(options =>
+{
+    options.UseInMemoryDatabase("BluntDb");
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] BluntContext db) =>
+{
+    var result = db.Database.EnsureCreated();
+
+    return "Hello world!" + "\n\n" + (result
+        ? "Db recently created"
+        : "Working with an exiting db");
+});
 
 app.Run();
