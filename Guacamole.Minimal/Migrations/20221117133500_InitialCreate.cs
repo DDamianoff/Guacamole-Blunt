de684d7efb68a7b56ad83881e0f35d293f2105e2
Dino Damianoff <75574873+DDamianoff@users.noreply.github.com>
2022-11-17 10:34:27 -0300

    First migration + moved context to Guacamole.Minimal project.

---
+++ Guacamole.Minimal/Migrations/20221117133500_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Guacamole.Minimal.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    DateCreated = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ideas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Content = table.Column<string>(type: "TEXT", nullable: false),
+                    DateCreated = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    DateModified = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Archived = table.Column<bool>(type: "INTEGER", nullable: false),
+                    CategoryId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ideas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ideas_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DateCreated", "Name" },
+                values: new object[] { 1, new DateOnly(2022, 11, 17), "Default" });
+
+            migrationBuilder.InsertData(
+                table: "Ideas",
+                columns: new[] { "Id", "Archived", "CategoryId", "Content", "DateCreated", "DateModified" },
+                values: new object[] { 1, false, 1, "My first idea!", new DateOnly(2022, 11, 17), new DateOnly(2022, 11, 17) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ideas_CategoryId",
+                table: "Ideas",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ideas");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
