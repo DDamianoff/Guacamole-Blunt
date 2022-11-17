354c2ec9a6ab4beb0286a9ce83ff0552827656f9
Dino Damianoff <75574873+DDamianoff@users.noreply.github.com>
2022-11-17 11:02:26 -0300

    Migration: dateonly to datetime

---
+++ Guacamole.Minimal/Migrations/20221117135203_DateOnlyToDateTime.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Guacamole.Minimal.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateOnlyToDateTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateModified",
+                table: "Ideas",
+                newName: "Modified");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Ideas",
+                newName: "Created");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "Categories",
+                newName: "Created");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2022, 11, 17, 10, 52, 3, 464, DateTimeKind.Local).AddTicks(9104));
+
+            migrationBuilder.UpdateData(
+                table: "Ideas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "Modified" },
+                values: new object[] { new DateTime(2022, 11, 17, 10, 52, 3, 464, DateTimeKind.Local).AddTicks(9446), new DateTime(2022, 11, 17, 10, 52, 3, 464, DateTimeKind.Local).AddTicks(9452) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Modified",
+                table: "Ideas",
+                newName: "DateModified");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Ideas",
+                newName: "DateCreated");
+
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Categories",
+                newName: "DateCreated");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateOnly(2022, 11, 17));
+
+            migrationBuilder.UpdateData(
+                table: "Ideas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateOnly(2022, 11, 17), new DateOnly(2022, 11, 17) });
+        }
+    }
+}
